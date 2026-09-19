b96f2a3ffe61e7ea9b2e7937d2599ed14ff90e45
Tirso Garcia <tgarciaib@gmail.com>
2026-09-19 02:44:33 +0200

    test: recover postgres child joins without broker

--- scripts/ci/integration-postgres.sh
+++ scripts/ci/integration-postgres.sh
@@ -22,5 +22,6 @@ RUST_TEST_THREADS=1 cargo test \
   --test postgres_statistics \
   --test postgres_ceremony_store \
   --test postgres_ceremony_ha \
+  --test postgres_children_recovery \
   --locked \
   -- --test-threads=1
